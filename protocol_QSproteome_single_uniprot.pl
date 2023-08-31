17ad19164e3132bd4c19a9a82d68229dfee62761
hschweke <hschweke@gmail.com>
2023-08-31 14:09:56 +0300

    workflow ok until r analysis

--- protocol_QSproteome_single_uniprot.pl
+++ protocol_QSproteome_single_uniprot.pl
@@ -7,14 +7,10 @@ my $JSON = $ARGV[1];
 my $OUTPATH = $ARGV[2];
 
 # Extract pdb id
-my $CODE = (fileparse($file_path, qr/\.[^.]*/))[0];
+my $CODE = (fileparse($PDBFILE, qr/\.[^.]*/))[0];
 print "code: $CODE";
 
-# Extract directory path
-my $directory_path = dirname($file_path);
-
-
-# Check if the directory exists
+# Check if the output directory exists
 if (!-d $OUTPATH) {
     # Create the directory
     eval {
@@ -37,10 +33,11 @@ require("/media/elusers/users/hugo/15_alphafold/37_revision_Cell/functions_get_c
 ## Calculate clashes
 my @PDBFILES = ($PDBFILE);             
 
-process_contacts(@PDBFILES, $CODE, 1);
+process_contacts(@PDBFILES, "$OUTPATH/$CODE", 1);
 
 #### STEP 2
 ### Calculate nodiso1, 2, 3 
-#my $CONTACTFILE = 
+my $CONTACTFILE = "$OUTPATH/$CODE"."_FULL.txt";
+print "$CONTACTFILE\n";
 #system("Rscript process_and_analyze_AF_model.R $PDBFILE $JSON $CONTACTFILE $OUTPATH")
 
