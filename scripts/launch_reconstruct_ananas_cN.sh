2f079774b3be7349ca59c97d79804329127c183f
hschweke <hschweke@gmail.com>
2023-09-11 10:40:38 +0300

    changed path to AnAnas to export variable

--- scripts/launch_reconstruct_ananas_cN.sh
+++ scripts/launch_reconstruct_ananas_cN.sh
@@ -5,7 +5,6 @@
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 echo -n "path to script: $SCRIPTPATH"
-#source ~/.bashrc
 
 
 pdb=$1
@@ -28,8 +27,7 @@ echo "symmetry av.rmsd clashscore" > $outfile
 
 for sym in 2 3 4 5 6 7 8 9 10 11 12
 do
-    rmsd=`${SCRIPTPATH}/../bin/ananas $pdb c$sym -C 100 | grep "Average RMSD" | awk '{ print $4 }' `
-    #lower=`awk -v a=$rmsd 'BEGIN { print (a <= 2.5) ? "YES" : "NO" }'`
+    rmsd=`$ANANAS $pdb c$sym -C 100 | grep "Average RMSD" | awk '{ print $4 }' `
     lower=`awk -v a=$rmsd 'BEGIN { print (a <= 7) ? "YES" : "NO" }'`
     clashscore="NA"
     echo -e "\n*************************\nrmsd : $rmsd\n*************************\n"
@@ -44,27 +42,26 @@ do
     elif [ $lower == "YES" ]
     then
         echo -e "\n*************************\nsymmetry c$sym has rmsd below 2.5A: $rmsd\n*************************\n"
+        
         # check if the exact same rmsd is in values already computed. 
         # if so it means that we are just testing a multiple of a smaller symmetry 
         # (for ex if C3 real symmetry, the C6 and C9 have same rmsd but with clashes).
         if [[ ! " ${listrmsd[*]} " =~ " ${rmsd:0:6} " ]]
         then
             echo "symmetry c$sym has rmsd below 2.5A: $rmsd"
+            
             # step 1: reconstruct the pdb of this specific symmetry
-            ${SCRIPTPATH}/../bin/ananas $pdb c$sym --symmetrize $outpath/${id%.pdb}_c${sym}.pdb
+            $ANANAS $pdb c$sym --symmetrize $outpath/${id%.pdb}_c${sym}.pdb
+            
             # step 2: assess clash score with phenix.clashscore
-            #clashscore=`/opt/phenix_1.20/phenix-1.20.1-4487/build/bin/phenix.clashscore $outpath/${id%.pdb}_c${sym}.pdb | grep "clashscore" | cut -d'=' -f2`
             clashscore=`$PHENIX_CLASHSCORE $outpath/${id%.pdb}_c${sym}.pdb | grep "clashscore" | cut -d'=' -f2`
+            
             rm $outpath/${id%.pdb}_c${sym}.pdb # removing symmetrized pdb after evaluating clashes
         fi
     fi
     
     # append rmsd to the array
     listrmsd+=(${rmsd:0:6})
-
-    #echo -e "\n\n*********************\n"
-    #echo -e "${listrmsd[*]}"
-    #echo -e "\n*********************\n\n"
 
     echo c$sym $rmsd $clashscore >> $outfile
 done
